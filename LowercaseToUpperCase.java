a95da73e5aba5843ca893b76e5a37bfe2489c3fd
Adithi0314 <113668713+Adithi0314@users.noreply.github.com>
2022-11-21 09:50:42 +0530

    Add files via upload

---
+++ LowercaseToUpperCase.java
@@ -0,0 +1,13 @@
+package javaprograms;
+
+public class LowercaseToUpperCase {
+
+	public static void main(String[] args) {
+		String str1="GOOD MORNING";
+		System.out.println("String value="+str1.toLowerCase());
+		
+		String str2="lowercase to Upper case";
+		System.out.println("String value="+str2.toUpperCase());
+	}
+
+}
