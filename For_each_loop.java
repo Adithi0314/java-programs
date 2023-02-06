4f91d04d20de79161d3ef36571c7be3413ad12eb
Adithi0314 <113668713+Adithi0314@users.noreply.github.com>
2023-02-06 10:19:11 +0530

    Add files via upload

---
+++ For_each_loop.java
@@ -0,0 +1,18 @@
+package javaprograms;
+
+public class For_each_loop {
+
+	public static void main(String[] args) {
+		
+		   int[] numbers = {3, 4, 5, -5, 0, 12};
+		   int sum = 0;
+
+		   for (int number: numbers) {
+		     sum += number;
+		   }
+		  
+		   System.out.println("Sum = " + sum);
+
+	}
+
+}
---
+++ For_loop_vs_For_each_loop.java
@@ -0,0 +1,10 @@
+package javaprograms;
+
+public class For_loop_vs_For_each_loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
